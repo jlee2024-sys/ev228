af7f6922765fcb3b596e14194b76708aa99d7933
jlee2024-sys <j_lee2024@coloradocollege.edu>
2025-11-06 21:20:56 -0700

    Added documentation to functions

--- fun_import_era5.py
+++ fun_import_era5.py
@@ -1,7 +1,14 @@
 import xarray as xr
 
 def import_era5(file_path='', var=''):
-    ''' Import ERA5 gridded data '''
+    ''' This function imports ERA5 gridded data and returns the data for a specific variable.
+    
+    Arguments:
+    file_path -- the path where the file is saved on the device (this includes the file name!)
+    var -- the variable of interest in the dataset that we wish to return.
+    
+    Outputs:
+    da -- the DataArray of the data from the variable of interest.'''
     ds = xr.open_dataset(file_path)
     da = ds[var]
 
--- fun_plot_era5.py
+++ fun_plot_era5.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot as plt
 
 def map(in_da, title='', cblabel='', out_path='', out_name=''):
-    ''' Plot map from 2D DataArray '''
+    ''' This function plots a map from a 2D DataArray.
+     
+      Arguments:
+      in_da -- the data array to be mapped.
+      title -- the title of the graph.
+      cblabel -- the label (units) for the colorbar.
+      out_path -- the path on to where the image is saved on the device.
+      out_name -- the name the image is saved under on the device.
+
+      Outputs:
+      None; saves an image to the device but doesn't return a value.
+      '''
     fig = plt.figure()
     ax = fig.add_subplot(111)
     lons = in_da.longitude
